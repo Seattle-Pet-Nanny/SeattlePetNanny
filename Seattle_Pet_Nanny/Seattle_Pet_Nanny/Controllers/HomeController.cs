16e155804664ca37f625de82ea77795d91a235bb
Brandon Little <brlittle86@gmail.com>
2017-12-14 11:52:01 -0800

    added routes for each controller action

--- Seattle_Pet_Nanny/Seattle_Pet_Nanny/Controllers/HomeController.cs
+++ Seattle_Pet_Nanny/Seattle_Pet_Nanny/Controllers/HomeController.cs
@@ -8,21 +8,25 @@ namespace Seattle_Pet_Nanny.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/home")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("/about")]
         public IActionResult About()
         {
             return View();
         }
 
+        [Route("/rates")]
         public IActionResult Rates()
         {
             return View();
         }
 
+        [Route("/contact")]
         public IActionResult Contact()
         {
             return View();
