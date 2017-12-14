3a0c3d00bca5cc2c00d225cff83f6455b7c659be
Brandon Little <brlittle86@gmail.com>
2017-12-14 11:46:39 -0800

    Created basic Home Controller with actions for each View

---
+++ Seattle_Pet_Nanny/Seattle_Pet_Nanny/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Seattle_Pet_Nanny.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Rates()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
